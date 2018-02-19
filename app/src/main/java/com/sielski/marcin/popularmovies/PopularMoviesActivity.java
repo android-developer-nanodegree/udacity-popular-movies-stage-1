c74d323a04028ce481a10f54efb94fb17c247611
Marcin Sielski <marcin.sielski@gmail.com>
2018-02-19 21:27:52 +0100

    Empty project.

---
+++ app/src/main/java/com/sielski/marcin/popularmovies/PopularMoviesActivity.java
@@ -0,0 +1,13 @@
+package com.sielski.marcin.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopularMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular_movies);
+    }
+}
